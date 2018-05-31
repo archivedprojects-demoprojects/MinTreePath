981e2428cf2fb26db7e25eade5dcf1a16d626d02
Jonathan Borg <jonathanborg1996@gmail.com>
2018-05-31 22:49:51 +0200

    Storing data read from file through lambda calculus (int format) Added print function for data read displaying

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,26 +5,36 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        List<String> content = file_read("data_small.txt");
-        System.out.println(content);
+        long startTime = System.nanoTime();
+        List<int[]> content = file_read("data_small.txt");
+        if(content != null) {
+            array_print(content);
+        }
+        long endTime = System.nanoTime();
+        long duration = (endTime - startTime);
+        System.out.println("Duration: "+duration/1000000+"ms");
     }
 
-    private static List<String> file_read(String file_name){
+    private static List<int[]> file_read(String file_name){
         String line;
-        List<String> content = new ArrayList<>();
+        int[] line_content;
+        List<int[]> content = new ArrayList<>();
         try {
             FileReader fileReader = new FileReader(file_name);
-
             BufferedReader bufferedReader = new BufferedReader(fileReader);
 
+            //TODO - check how to optimise loop below
             while((line = bufferedReader.readLine()) != null) {
-//                System.out.println(line);
-                content.add(line);
+                //Using Lambda Expressions
+                line_content = Arrays.stream(line.split(" ")).mapToInt(Integer::parseInt).toArray();
+                //line_content = Integer.parseInt(line.split(" ")); - Old approach using Strings
+                content.add(line_content);
             }
 
             bufferedReader.close();
@@ -36,6 +46,16 @@ public class Main {
         }catch(IOException ex) {
             System.out.println("Error reading file '" + file_name + "'");
             return null;
+        }
+    }
+
+    //TODO - Function to be optimised - remove for loop, use higher order
+    private static void array_print(List<int[]> content){
+        for (int[] rows : content) {
+            for (int values : rows) {
+                System.out.print(values + " ");
+            }
+            System.out.println();
         }
     }
 
