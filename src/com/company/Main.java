0d0bfcc9b87e0b0fb1b347b74553352552f00c30
Jonathan Borg <jonathanborg1996@gmail.com>
2018-05-31 22:15:39 +0200

    Reading from file and storing data read

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,42 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        List<String> content = file_read("data_small.txt");
+        System.out.println(content);
     }
+
+    private static List<String> file_read(String file_name){
+        String line;
+        List<String> content = new ArrayList<>();
+        try {
+            FileReader fileReader = new FileReader(file_name);
+
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            while((line = bufferedReader.readLine()) != null) {
+//                System.out.println(line);
+                content.add(line);
+            }
+
+            bufferedReader.close();
+            return content;
+
+        }catch(FileNotFoundException ex) {
+            System.out.println("Unable to open file '" + file_name + "'");
+            return null;
+        }catch(IOException ex) {
+            System.out.println("Error reading file '" + file_name + "'");
+            return null;
+        }
+    }
+
 }
