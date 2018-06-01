cea608680559c806b05f7c62eea9b000cabde747
jonathan-borg96 <jonathanborg1996@gmail.com>
2018-06-01 20:10:49 +0200

    Implementing first Naive Bottom Up approach

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,15 +6,16 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
         long startTime = System.nanoTime();
-        List<int[]> content = file_read("data_small.txt");
+        List<int[]> content = file_read("data_test.txt");
         if(content != null) {
-            array_print(content);
+            bottom_up_approach_naive(content);
         }
         long endTime = System.nanoTime();
         long duration = (endTime - startTime);
@@ -59,4 +60,36 @@ public class Main {
         }
     }
 
+    private static void bottom_up_approach_naive(List<int[]> content){
+        List<int[]> bottom = new ArrayList<>(content);
+        List<int[]> shortest_paths = new ArrayList<>();
+
+        Collections.reverse(bottom);
+        array_print(bottom);
+        array_print(content);
+
+        //from the leave we can go parent having the same index or index - 1
+        //ie if we are at leaf 4, I can go to the following parent notes: 3,4
+        for (int i =0; i <bottom.size(); i++) {
+            /*            if(i == 0){
+                shortest_paths.add(bottom.get(i));
+            }else */
+            if(i != bottom.size()-1) {
+                for (int j = 0; j < bottom.get(i).length; j++) {
+                    int current_value = bottom.get(i)[j];
+                    if (j == 0) {
+                        System.out.println("Edge L:" + current_value);
+                    } else if (j == bottom.get(i).length - 1) { //first and last cases (far edges - have only 1 parent
+                        System.out.println("Edge R:" + current_value);
+                    } else {
+                        System.out.println("Leaf:" + current_value);
+                    }
+                }
+            }else{
+                int current_value = bottom.get(i)[0];
+                System.out.println("Parent: "+ current_value);
+            }
+            System.out.println();
+        }
+    }
 }
